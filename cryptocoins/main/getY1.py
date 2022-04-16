4191dc24ff0ef7efe20de7fa9d43ca9911312d4a
zovenor <romanenyasasha@gmail.com>
2022-04-16 17:48:58 +0300

    Updated a chat bot

--- cryptocoins/main/getY1.py
+++ cryptocoins/main/getY1.py
@@ -1,10 +1,15 @@
-import telegram_send
+import telebot
 import openpyxl
+
+BOT_TOKEN = '5397252848:AAHg9ZE9HLBflvI-uAUWGgDHFYVk46dr-ng'
+CHANNEL_NAME = '@cryptocoinstest'
 
 
 def send_message():
     wb = openpyxl.load_workbook('result.xlsx', data_only=True)
     sheet = wb.active
     data = sheet['Y1'].value
+
+    bot = telebot.TeleBot(BOT_TOKEN)
     if data != None:
-        telegram_send.send(messages=[str(data)])
+        bot.send_message(CHANNEL_NAME, str(data))
